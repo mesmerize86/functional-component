7712dfcb4c50c1f281b98f2848f20dfb7b5e35b2
Kishor Maharjan <lasataya@gmail.com>
2018-06-12 17:08:43 +1000

    removed commented code from image source file

--- src/shared-components/images/image-source.js
+++ src/shared-components/images/image-source.js
@@ -1,10 +1,5 @@
 export const defaultPath = '/assets/images/';
 
-// export const imageNotFound = ( defaultPath ) =>({
-//   'imagePath' : defaultPath,
-//   'imageName' : 'no-image-available.jpg'
-// })
-
 export const imageNotFound = {
   'imagePath' : defaultPath,
   'imageName' : 'no-image-available.jpg'
